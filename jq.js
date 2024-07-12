67962a81a15fe13c553fed78d1f9bc092b518c04
AbivarsanR <abivarsanr4104@gmail.com>
2024-07-12 19:57:37 +0530

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,21 @@
+$("#second").hide();
+$("#third").hide();
+
+function Home(){
+$("#first").show()
+$("#second").hide();
+$("#third").hide();
+}
+
+function about(){
+    $("#first").hide();
+    $("#third").hide();
+   $("#second").show()
+
+}
+function Contact(){
+    $("#first").hide();
+    $("#third").show();
+   $("#second").hide()
+
+}
