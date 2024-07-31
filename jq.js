e1f4ee94674c788db7777306500a4855762d0e7d
AbivarsanR <abivarsanr4104@gmail.com>
2024-07-31 16:30:00 +0530

    Update jq.js

--- jq.js
+++ jq.js
@@ -6,8 +6,13 @@ function sidebarexit(){
 }
 
 function enter(){
-
+    $("#nav").css('display','flex');
     $("#header1").css('display','flex');
     $("#enter").hide();
 }
 
+function hide(){
+    $("#nav").css('display','none');
+    $("#header1").css('display','none');
+    $("#enter").show();
+}
