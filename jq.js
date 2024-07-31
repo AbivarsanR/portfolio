27c7b6a0bc37be86d56f3c0f89ee948a98b8ea96
AbivarsanR <abivarsanr4104@gmail.com>
2024-07-31 16:15:39 +0530

    Update jq.js

--- jq.js
+++ jq.js
@@ -10,3 +10,7 @@ function enter(){
     $("#header1").css('display','flex');
     $("#enter").hide();
 }
+
+document.addEventListener('touchstart', function() {
+    document.getElementById('header1').style.display = 'none';
+});
